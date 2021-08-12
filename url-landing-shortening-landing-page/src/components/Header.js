5d9a94f2404d574f5d6350b5df1ad66b714792c0
Julaxe <julaxe8@gmail.com>
2021-08-12 13:02:55 -0400

    header done for mobile

---
+++ url-landing-shortening-landing-page/src/components/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "../styles/header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+function NavLink(props) {
+  return <div className={props.className}>{props.children}</div>;
+}
+function NavBarDesktop(props) {
+  return <nav className="nav-desktop"></nav>;
+}
+function NavMenu(props) {
+  return (
+    <nav className="nav-menu">
+      <NavLink className="nav-menu-link">Features</NavLink>
+      <NavLink className="nav-menu-link">Pricing</NavLink>
+      <NavLink className="nav-menu-link">Resources</NavLink>
+      <div className="white-line"></div>
+      <NavLink className="nav-menu-link">Login</NavLink>
+      <NavLink className="nav-menu-signUp">Sign Up</NavLink>
+    </nav>
+  );
+}
+function MenuIcon(props) {
+  const [menu, SetMenu] = React.useState(false);
+
+  function handleOnClick() {
+    SetMenu(!menu);
+  }
+  return (
+    <div className="menu">
+      <div className="menu-icon" onClick={handleOnClick}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      {menu ? props.children : ""}
+    </div>
+  );
+}
+
+export default function Header() {
+  return (
+    <header>
+      <div className="title">Shortly</div>
+      <div className="nav">
+        <NavBarDesktop />
+        <MenuIcon>
+          <NavMenu />
+        </MenuIcon>
+      </div>
+    </header>
+  );
+}
