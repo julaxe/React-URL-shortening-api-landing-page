1f75fd3bb97328f6ca8e2add9d98323a9be402b8
Julaxe <julaxe8@gmail.com>
2021-08-12 18:11:48 -0400

    Body content, and footer completed for mobile

--- url-landing-shortening-landing-page/src/components/Body.js
+++ url-landing-shortening-landing-page/src/components/Body.js
@@ -1,5 +1,93 @@
 import React from "react";
+import "../styles/body.css";
+import Illustration from "../images/illustration-working.svg";
+import IconBrandRecognition from "../images/icon-brand-recognition.svg";
+import IconDetailedRecords from "../images/icon-detailed-records.svg";
+import IconFullyCustomizable from "../images/icon-fully-customizable.svg";
 
+function GetStarted(props) {
+  return (
+    <div className="get-started">
+      <img src={Illustration} alt="illustration of a person working" />
+      <div className="presentation">
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <div className="get-started-icon">Get Started</div>
+      </div>
+    </div>
+  );
+}
+function ShortenIt(props) {
+  function Search(props) {
+    return (
+      <div className="search">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Shorten a link here..."
+        />
+        <button className="search-button">Shorten it!</button>
+      </div>
+    );
+  }
+  function AdvancedStatistics(props) {
+    function Statistics(props) {
+      return (
+        <div className="statistics">
+          <div className="statistics-logo">
+            <img src={props.logo} alt="logo" />
+          </div>
+          <h3>{props.title}</h3>
+          <p>{props.children}</p>
+        </div>
+      );
+    }
+    return (
+      <div className="advanced-statistics">
+        <h2>Advanced Statistics</h2>
+        <p>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+        <Statistics logo={IconBrandRecognition} title="Brand Recognition">
+          Boost your brand recognition with each click. Generic links don't mean
+          a thing. Branded links help instill confidence in your content.
+        </Statistics>
+        <Statistics logo={IconDetailedRecords} title="Detailed Records">
+          Gain insights into who is clicking your links. Knowing when and where
+          people engage with your content helps inform better decisions.
+        </Statistics>
+        <Statistics logo={IconFullyCustomizable} title="Fully Customizable">
+          Improve brand awareness and content discoverability through
+          customizable links, supercharging audience engagement.
+        </Statistics>
+      </div>
+    );
+  }
+  function BoostYourLinks(props) {
+    return (
+      <div className="boost-your-links-today">
+        <h2>Boost your links today</h2>
+        <div className="get-started-icon"> Get Started</div>
+      </div>
+    );
+  }
+  return (
+    <div className="shorten-it">
+      <Search />
+      <AdvancedStatistics />
+      <BoostYourLinks />
+    </div>
+  );
+}
 export default function Body() {
-  return <div>Body</div>;
+  return (
+    <div className="body">
+      <GetStarted />
+      <ShortenIt />
+    </div>
+  );
 }
--- url-landing-shortening-landing-page/src/components/Footer.js
+++ url-landing-shortening-landing-page/src/components/Footer.js
@@ -1,5 +1,58 @@
 import React from "react";
+import "../styles/footer.css";
+import {
+  FaFacebookSquare,
+  FaTwitter,
+  FaPinterest,
+  FaInstagram,
+} from "react-icons/fa";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  function FooterLinks(props) {
+    return (
+      <div className="footer-links">
+        <h3>{props.title}</h3>
+        <div className="links-list">{props.children}</div>
+      </div>
+    );
+  }
+  function FooterLink(props) {
+    return <div className="footer-link">{props.children}</div>;
+  }
+  function SocialMedia(props) {
+    function LogoMedia(props) {
+      return <div className="logo-media">{props.logo}</div>;
+    }
+    return (
+      <div className="social-media">
+        <FaFacebookSquare />
+        <FaTwitter />
+        <FaPinterest />
+        <FaInstagram />
+      </div>
+    );
+  }
+
+  return (
+    <footer>
+      <h1>Shortly</h1>
+      <FooterLinks title="Features">
+        <FooterLink>Link Shortening</FooterLink>
+        <FooterLink>Branded Links</FooterLink>
+        <FooterLink>Analytics</FooterLink>
+      </FooterLinks>
+      <FooterLinks title="Resources">
+        <FooterLink>Blog</FooterLink>
+        <FooterLink>Developers</FooterLink>
+        <FooterLink>Support</FooterLink>
+      </FooterLinks>
+      <FooterLinks title="Company">
+        <FooterLink>About</FooterLink>
+        <FooterLink>Our Team</FooterLink>
+        <FooterLink>Careers</FooterLink>
+        <FooterLink>Contact</FooterLink>
+      </FooterLinks>
+      <SocialMedia />
+    </footer>
+  );
 }
